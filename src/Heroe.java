8bd380281ca9f8c3f4eae2da24f128ab0084ff59
Felipe <jgonzalezc@uniempresarial.edu.co>
2026-09-03 20:07:54 -0500

    Creamos los constructores de la super clase Heroe.java

--- src/Heroe.java
+++ src/Heroe.java
@@ -4,4 +4,32 @@ public class Heroe {
     private int edad;
     private double poderNivel;
     private boolean esActivo;
+
+    public Heroe() {
+        this("Héroe sin identidad asignada");
+    }
+
+    /**
+     * Constructor de 1 parámetro
+     */
+    public Heroe(String nombreReal) {
+        this(nombreReal, 18);
+    }
+
+    /**
+     * Constructor de 2 parámetros
+     */
+    public Heroe(String nombreReal, int edad) {
+        this(nombreReal, edad, 1.0, true);
+    }
+
+    /**
+     * Constructor completo
+     */
+    public Heroe(String nombreReal, int edad, double poderNivel, boolean esActivo) {
+        this.nombreReal = nombreReal;
+        this.edad = edad;
+        this.poderNivel = poderNivel;
+        this.esActivo = esActivo;
+    }
 }
