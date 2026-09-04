2cc508a3d7ba679045d61510c935135f19e360cd
Felipe <jgonzalezc@uniempresarial.edu.co>
2026-09-03 19:52:31 -0500

    Creamos la clase Heroe.java y le asignamos los atributos

---
+++ src/Heroe.java
@@ -0,0 +1,7 @@
+public class Heroe {
+    // 4 atributos de tipos de datos diferentes entre sí
+    private String nombreReal;
+    private int edad;
+    private double poderNivel;
+    private boolean esActivo;
+}
