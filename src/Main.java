5a1e85831eef80369c4eeb9f88ffa77e91e00445
Felipe <jgonzalezc@uniempresarial.edu.co>
2026-09-03 21:19:43 -0500

    En els segundo constructor el heroe veterano ya tiene toda su infomarcion basica completada

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,12 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("=== REGISTRO DE SUPERHEROES ===\n");
 
-        //(recién ingresa al programa de reclutas).
+        //recién ingresa al programa de reclutas
         Heroe heroeSinAsignar = new Heroe();
+
+        //Toda la informacion basica
+        Heroe heroeVeterano = new Heroe("Marco Salgado", 45, 7.8, true);
+
 
     }
 }
