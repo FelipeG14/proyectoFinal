1392b0247f3fc564da2bd8d4998baab1c4f386e4
Felipe <jgonzalezc@uniempresarial.edu.co>
2026-09-03 20:49:53 -0500

    creamos los constructorers y los getters y setters correspondientes a cada atributo

--- src/HeroeTanque.java
+++ src/HeroeTanque.java
@@ -5,4 +5,73 @@ public class HeroeTanque extends Heroe {
     private double pesoArmadura;
     private boolean tieneEscudo;
     private String materialArmadura;
+
+    /**
+     * Constructor de solo nombre
+     */
+    public HeroeTanque(String nombreReal) {
+        super(nombreReal);
+        this.nivelResistencia = 50;
+        this.pesoArmadura = 0.0;
+        this.tieneEscudo = false;
+        this.materialArmadura = "Desconocido";
+    }
+
+    /**
+     * Constructor con dos datos heredados
+     */
+    public HeroeTanque(String nombreReal, int edad, int nivelResistencia, String materialArmadura) {
+        super(nombreReal, edad);
+        this.nivelResistencia = nivelResistencia;
+        this.pesoArmadura = 100.0;
+        this.tieneEscudo = true;
+        this.materialArmadura = materialArmadura;
+    }
+
+    /**
+     * Constructor completo
+     */
+    public HeroeTanque(String nombreReal, int edad, double poderNivel, boolean esActivo,
+                       int nivelResistencia, double pesoArmadura,
+                       boolean tieneEscudo, String materialArmadura) {
+        super(nombreReal, edad, poderNivel, esActivo);
+        this.nivelResistencia = nivelResistencia;
+        this.pesoArmadura = pesoArmadura;
+        this.tieneEscudo = tieneEscudo;
+        this.materialArmadura = materialArmadura;
+    }
+
+    // Getters y setters propios
+
+    public int getNivelResistencia() {
+        return nivelResistencia;
+    }
+
+    public void setNivelResistencia(int nivelResistencia) {
+        this.nivelResistencia = nivelResistencia;
+    }
+
+    public double getPesoArmadura() {
+        return pesoArmadura;
+    }
+
+    public void setPesoArmadura(double pesoArmadura) {
+        this.pesoArmadura = pesoArmadura;
+    }
+
+    public boolean isTieneEscudo() {
+        return tieneEscudo;
+    }
+
+    public void setTieneEscudo(boolean tieneEscudo) {
+        this.tieneEscudo = tieneEscudo;
+    }
+
+    public String getMaterialArmadura() {
+        return materialArmadura;
+    }
+
+    public void setMaterialArmadura(String materialArmadura) {
+        this.materialArmadura = materialArmadura;
+    }
 }
