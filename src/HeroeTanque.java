df87d5c0a0c4e27377f39bd6c9dbdc08f7224483
Felipe <jgonzalezc@uniempresarial.edu.co>
2026-09-03 20:41:30 -0500

    Creamos otra subclase con sus atributos unicos

---
+++ src/HeroeTanque.java
@@ -0,0 +1,8 @@
+public class HeroeTanque extends Heroe {
+
+    // 4 atributos propios, de tipos diferentes entre sí
+    private int nivelResistencia;
+    private double pesoArmadura;
+    private boolean tieneEscudo;
+    private String materialArmadura;
+}
