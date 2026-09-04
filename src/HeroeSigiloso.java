14b5877f62f119eb94f8c93a5a13f32b9d674220
Felipe <jgonzalezc@uniempresarial.edu.co>
2026-09-03 20:57:51 -0500

    Creamos la calse HeroeSigiloso.java y le asignamos los atributos unicos de esta clase

---
+++ src/HeroeSigiloso.java
@@ -0,0 +1,10 @@
+public class HeroeSigiloso extends Heroe {
+
+    // 4 atributos propios, de tipos diferentes entre sí
+    private double nivelInvisibilidad;
+    private int numArmasSecretas;
+    private boolean usaGadgets;
+    private char rangoSigilo;
+
+
+}
