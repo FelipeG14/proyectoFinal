6baf1b28d3630b2d2e6a4d7d655620d4139d6b77
Felipe <jgonzalezc@uniempresarial.edu.co>
2026-09-03 20:36:24 -0500

    Creamos cada constructor y ponemos los setters y getters

--- src/HeroeVolador.java
+++ src/HeroeVolador.java
@@ -5,4 +5,73 @@ public class HeroeVolador extends Heroe{
     private int altitudMaxima;
     private boolean puedeCargarPasajeros;
     private char clasificacionVuelo;
+
+    /**
+     * Constructor de solo nombre
+     */
+    public HeroeVolador(String nombreReal) {
+        super(nombreReal);
+        this.velocidadMaxima = 0.0;
+        this.altitudMaxima = 0;
+        this.puedeCargarPasajeros = false;
+        this.clasificacionVuelo = 'C';
+    }
+
+    /**
+     * Constructor con dos datos heredados
+     */
+    public HeroeVolador(String nombreReal, int edad, double velocidadMaxima) {
+        super(nombreReal, edad);
+        this.velocidadMaxima = velocidadMaxima;
+        this.altitudMaxima = 1000;
+        this.puedeCargarPasajeros = false;
+        this.clasificacionVuelo = 'B';
+    }
+
+    /**
+     * Constructor completo
+     */
+    public HeroeVolador(String nombreReal, int edad, double poderNivel, boolean esActivo,
+                        double velocidadMaxima, int altitudMaxima,
+                        boolean puedeCargarPasajeros, char clasificacionVuelo) {
+        super(nombreReal, edad, poderNivel, esActivo);
+        this.velocidadMaxima = velocidadMaxima;
+        this.altitudMaxima = altitudMaxima;
+        this.puedeCargarPasajeros = puedeCargarPasajeros;
+        this.clasificacionVuelo = clasificacionVuelo;
+    }
+
+    // Getters y setters
+
+    public double getVelocidadMaxima() {
+        return velocidadMaxima;
+    }
+
+    public void setVelocidadMaxima(double velocidadMaxima) {
+        this.velocidadMaxima = velocidadMaxima;
+    }
+
+    public int getAltitudMaxima() {
+        return altitudMaxima;
+    }
+
+    public void setAltitudMaxima(int altitudMaxima) {
+        this.altitudMaxima = altitudMaxima;
+    }
+
+    public boolean isPuedeCargarPasajeros() {
+        return puedeCargarPasajeros;
+    }
+
+    public void setPuedeCargarPasajeros(boolean puedeCargarPasajeros) {
+        this.puedeCargarPasajeros = puedeCargarPasajeros;
+    }
+
+    public char getClasificacionVuelo() {
+        return clasificacionVuelo;
+    }
+
+    public void setClasificacionVuelo(char clasificacionVuelo) {
+        this.clasificacionVuelo = clasificacionVuelo;
+    }
 }
